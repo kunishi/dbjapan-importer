a9582e82d3c98f3634bf7f6f05f0400254480307
Takeo Kunishima <t.kunishi@gmail.com>
2017-11-06 17:13:55 +0900

    a script for importing all dbjapan mails using the original importer.

---
+++ dbjapan-importer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DBJAPAN_DIR=$HOME/git/dbjapan
+
+for year in `ls $DBJAPAN_DIR/20*.txt`; do
+  for file in `cat $year`; do
+    echo $file
+    ruby mailimporter.rb $DBJAPAN_DIR/$file
+  done
+done
+
+for file in `ls $DBJAPAN_DIR/06_Dbjapan_mlmmj`; do
+  echo $file
+  ruby mailimporter.rb $DBJAPAN_DIR/06_Dbjapan_mlmmj/$file
+done
